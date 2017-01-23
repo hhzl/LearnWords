eb84483519e106e8260bdb644532bb38928251cf
e1r0nd <a.merezhanyi@gmail.com>
2017-01-23 02:17:53 +0100

    fixed main import

--- app/js/main.js
+++ app/js/main.js
@@ -6,25 +6,21 @@
 **************************************************/
 'use strict';
 
-import LW from './utils/LW';
-console.log(LW);
-let LWdb = new LW('LWdb');
-console.log(LW);
-console.log(LWdb.isLocalStorageAvailable());
+import LWClass from './utils/LW';
+// console.log(LW);
+const LW = new LWClass('LWdb');
+// console.log(LW);
+console.log(LW.isLocalStorageAvailable());
 
-import Settings from '../components/settings/settings';
-// let settings = new Settings();
-// console.log(Settings);
+import SettingsClass from './components/settings/settings';
+const Settings = new SettingsClass();
 
 import Utils from './utils/utils';
-// console.log(Utils);
-// import Memorystore from './js/utils/memorystore';
-// import Navigation from './js/utils/navigation';
-// import Local from './js/local/local';
-// import Learn from './js/actions/learn';
-// import Repeat from './js/actions/repeat';
-// import Vocabulary from './js/actions/vocabulary';
-
+// import Memorystore from './utils/memorystore';
+// import Navigation from './utils/navigation';
+// import Local from './local/local';
+// import Actions from './actions/actions';
+let a;
 if ('development' === NODE_ENV) {
   console.log(`development environment ${NODE_ENV}`);
 }
@@ -32,13 +28,13 @@ if ('development' === NODE_ENV) {
 Settings.getSettings();
 
 // set user saved local
-if (local.currentLocal !== $('[data-type=lang-select].selected').data('lang')) {
-	$('[data-lang=' + local.currentLocal + ']').click();
-};
+//if (local.currentLocal !== $('[data-type=lang-//select].selected').data('lang')) {
+//	$('[data-lang=' + local.currentLocal + ']').click();
+//};
 
-// Vocabulary.viewWord();
-// Learn.recountIndexLearn();
-// Learn.showWord();
-// Repeat.recountIndexRepeat();
-// Repeat.showWord();
-// Utils.closeMobMenu();
+Vocabulary.viewWord();
+Learn.recountIndexLearn();
+Learn.showWord();
+Repeat.recountIndexRepeat();
+Repeat.showWord();
+Utils.closeMobMenu();
