0c2c1384f37949d324e2583726b5a800d5367eff
e1r0nd <a.merezhanyi@gmail.com>
2017-01-23 10:57:48 +0100

    added Memorystore #25

--- app/js/utils/memorystore.js
+++ app/js/utils/memorystore.js
@@ -1,29 +1,104 @@
 /**************************************************
-* Learn Words // memorystore.js
-*
-* Load initial set of German words with
-* English translation.
-*
-**************************************************/
+ * Learn Words // memorystore.js
+ * coded by Anatol Marezhanyi aka e1r0nd//[CRG] - January 2017
+ * http://linkedin.com/in/merezhany/ a.merezhanyi@gmail.com
+ * Placed in public domain.
+ **************************************************/
+'use strict';
 
-"use strict";
-
-if (LW.isOK && LW.isEmpty) {
-        console.log('memorystore: start loading words');
-
-LW.loadWords(
-[
-{"index":"index1","word":"das Auto","translate":"car","step":0,"date":0},
-{"index":"index2","word":"laufen","translate":"run","step":0,"date":0},
-{"index":"index3","word":"alt","translate":"old","step":0,"date":0},
-{"index":"index4","word":"krank","translate":"sick","step":0,"date":0},{"index":"index5","word":"heute","translate":"today","step":0,"date":0},{"index":"index6","word":"schreiben","translate":"write","step":0,"date":0},{"index":"index7","word":"hell","translate":"light","step":0,"date":0},
-{"index":"index8","word":"reich","translate":"rich","step":0,"date":0},
-{"index":"index9","word":"süß","translate":"sweet","step":1,"date":0},
-{"index":"index10","word":"weiblich","translate":"female","step":1,"date":0},{"index":"index11","word":"bestellen","translate":"order","step":1,"date":0},
-{"index":"index12","word":"kalt","translate":"cold","step":2,"date":0},
-{"index":"index13","word":"sauer","translate":"sour","step":2,"date":0},
-{"index":"index14","word":"fliegen","translate":"fly","step":3,"date":0}
-]
-);
-
-}
+export const Memorystore = [
+  {
+    'index': 'index1',
+    'word': 'das Auto',
+    'translate': 'car',
+    'step': 0,
+    'date': 0
+  },
+  {
+    'index': 'index2',
+    'word': 'laufen',
+    'translate': 'run',
+    'step': 0,
+    'date': 0
+  },
+  {
+    'index': 'index3',
+    'word': 'alt',
+    'translate': 'old',
+    'step': 0,
+    'date': 0
+  },
+  {
+    'index': 'index4',
+    'word': 'krank',
+    'translate': 'sick',
+    'step': 0,
+    'date': 0
+  }, {
+    'index': 'index5',
+    'word': 'heute',
+    'translate': 'today',
+    'step': 0,
+    'date': 0
+  }, {
+    'index': 'index6',
+    'word': 'schreiben',
+    'translate': 'write',
+    'step': 0,
+    'date': 0
+  }, {
+    'index': 'index7',
+    'word': 'hell',
+    'translate': 'light',
+    'step': 0,
+    'date': 0
+  },
+  {
+    'index': 'index8',
+    'word': 'reich',
+    'translate': 'rich',
+    'step': 0,
+    'date': 0
+  },
+  {
+    'index': 'index9',
+    'word': 'süß',
+    'translate': 'sweet',
+    'step': 1,
+    'date': 0
+  },
+  {
+    'index': 'index10',
+    'word': 'weiblich',
+    'translate': 'female',
+    'step': 1,
+    'date': 0
+  }, {
+    'index': 'index11',
+    'word': 'bestellen',
+    'translate': 'order',
+    'step': 1,
+    'date': 0
+  },
+  {
+    'index': 'index12',
+    'word': 'kalt',
+    'translate': 'cold',
+    'step': 2,
+    'date': 0
+  },
+  {
+    'index': 'index13',
+    'word': 'sauer',
+    'translate': 'sour',
+    'step': 2,
+    'date': 0
+  },
+  {
+    'index': 'index14',
+    'word': 'fliegen',
+    'translate': 'fly',
+    'step': 3,
+    'date': 0
+  }
+];
