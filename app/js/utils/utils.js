f60bf1708246f340a6057c0ac767bb427623c076
e1r0nd <a.merezhanyi@gmail.com>
2017-01-24 19:01:47 +0100

    utils: es5 to es6 #25

--- app/js/utils/utils.js
+++ app/js/utils/utils.js
@@ -3,63 +3,62 @@
  * coded by Anatolii Marezhanyi aka e1r0nd//[CRG] - March 2014
  * http://linkedin.com/in/merezhany/ e1r0nd.crg@gmail.com
  * Placed in public domain.
- **************************************************/
-'use strict';
+**************************************************/
+var Utils = {};
 
-if (typeof (Utils) == 'undefined' || Utils == null || !Utils) {
-  var Utils = {};
+Utils = {
+  isNumber(str, withDot) {
+    //validate filed for number value
+    const NumberReg = /^\d+$/;
+    const NumberWithDotReg = /^[-+]?[0-9]*\.?[0-9]+$/;
 
-  Utils = {
-    isNumber: function (str, withDot) { //validate filed for number value
-      var NumberReg = /^\d+$/,
-        NumberWithDotReg = /^[-+]?[0-9]*\.?[0-9]+$/;
+    return withDot ? NumberWithDotReg.test(str) : NumberReg.test(str);
+  },
 
-      return withDot ? NumberWithDotReg.test(str) : NumberReg.test(str);
-    },
+  clearFields() {
+    $('.form-group').each((i, node) => { //clear all error styles
+      $(node).removeClass('has-error');
+    });
+    $('#errorSettings').addClass('nodisplay');
+  },
 
-    clearFields: function () {
-      $('.form-group').each(function (i, node) { //clear all error styles
-        $(node).removeClass('has-error');
-      });
-      $('#errorSettings').addClass('nodisplay');
-    },
+  setFieldError(self) { //set the error style for the current input field
+    $(self).addClass('has-error');
+    return true;
+  },
 
-    setFieldError: function (self) { //set the error style for the current input field
-      $(self).addClass('has-error');
-      return true;
-    },
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  },
 
-    getRandomInt: function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    },
+  getToday(fullDate) {
+    const now = new Date();
 
-    getToday: function (fullDate) {
-      var now = new Date();
-
-      if (fullDate) {
-        return new Date().valueOf();
-      } else {
-        return new Date(now.getFullYear(), now.getMonth(), now.getDate()).valueOf();
-      }
-    },
-
-    closeMobMenu: function () {
-      if ($('#bs-example-navbar-collapse-1').hasClass('in')) {
-        $('#navbarToggle').click();
-      }
-    },
-
-    shuffle: function (a) {
-      var j, x, i;
-      for (i = a.length; i; i--) {
-        j = Math.floor(Math.random() * i);
-        x = a[i - 1];
-        a[i - 1] = a[j];
-        a[j] = x;
-      }
+    if (fullDate) {
+      return new Date().valueOf();
+    } else {
+      return new Date(now.getFullYear(), now.getMonth(), now.getDate()).valueOf();
     }
-  };
-}
+  },
+
+  closeMobMenu() {
+    if ($('#bs-example-navbar-collapse-1').hasClass('in')) {
+      $('#navbarToggle').click();
+    }
+  },
+
+  shuffle(a) {
+    let j;
+    let x;
+    let i;
+    for (i = a.length; i; i--) {
+      j = Math.floor(Math.random() * i);
+      x = a[i - 1];
+      a[i - 1] = a[j];
+      a[j] = x;
+    }
+  }
+};
 
 if (typeof module !== 'undefined' && module.exports != null) {
     exports.Utils = Utils;
