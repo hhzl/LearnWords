452840163b85ba41ccb4c06be99da123e48e38d2
e1r0nd <a.merezhanyi@gmail.com>
2017-02-03 12:35:32 +0100

    added eslint

--- app/data/loadWords-ge-ru.js
+++ app/data/loadWords-ge-ru.js
@@ -1,99 +1,99 @@
 /**************************************************
  * Learn Words //  loadWords-ge-ru.js
- * 
+ *
  * Load initial set of German words with Russian translation.
  **************************************************/
 
-"use strict";
+'use strict';
 
 if (LW.db.isOK && LW.db.isEmpty) {
     console.log('memorystore: start loading words');
 
     LW.db.loadWords(
         [{
-            "index": "index1",
-            "word": "das Auto",
-            "translate": "автомобиль",
-            "step": 0,
-            "date": 0
+            'index': 'index1',
+            'word': 'das Auto',
+            'translate': 'автомобиль',
+            'step': 0,
+            'date': 0
         }, {
-            "index": "index2",
-            "word": "laufen",
-            "translate": "бежать",
-            "step": 0,
-            "date": 0
+            'index': 'index2',
+            'word': 'laufen',
+            'translate': 'бежать',
+            'step': 0,
+            'date': 0
         }, {
-            "index": "index3",
-            "word": "alt",
-            "translate": "старый",
-            "step": 0,
-            "date": 0
+            'index': 'index3',
+            'word': 'alt',
+            'translate': 'старый',
+            'step': 0,
+            'date': 0
         }, {
-            "index": "index4",
-            "word": "krank",
-            "translate": "больной",
-            "step": 0,
-            "date": 0
+            'index': 'index4',
+            'word': 'krank',
+            'translate': 'больной',
+            'step': 0,
+            'date': 0
         }, {
-            "index": "index5",
-            "word": "heute",
-            "translate": "сегодня",
-            "step": 0,
-            "date": 0
+            'index': 'index5',
+            'word': 'heute',
+            'translate': 'сегодня',
+            'step': 0,
+            'date': 0
         }, {
-            "index": "index6",
-            "word": "schreiben",
-            "translate": "писать",
-            "step": 0,
-            "date": 0
+            'index': 'index6',
+            'word': 'schreiben',
+            'translate': 'писать',
+            'step': 0,
+            'date': 0
         }, {
-            "index": "index7",
-            "word": "hell",
-            "translate": "светлый",
-            "step": 0,
-            "date": 0
+            'index': 'index7',
+            'word': 'hell',
+            'translate': 'светлый',
+            'step': 0,
+            'date': 0
         }, {
-            "index": "index8",
-            "word": "reich",
-            "translate": "богатый",
-            "step": 0,
-            "date": 0
+            'index': 'index8',
+            'word': 'reich',
+            'translate': 'богатый',
+            'step': 0,
+            'date': 0
         }, {
-            "index": "index9",
-            "word": "süß",
-            "translate": "сладкий",
-            "step": 1,
-            "date": 0
+            'index': 'index9',
+            'word': 'süß',
+            'translate': 'сладкий',
+            'step': 1,
+            'date': 0
         }, {
-            "index": "index10",
-            "word": "weiblich",
-            "translate": "женский",
-            "step": 1,
-            "date": 0
+            'index': 'index10',
+            'word': 'weiblich',
+            'translate': 'женский',
+            'step': 1,
+            'date': 0
         }, {
-            "index": "index11",
-            "word": "bestellen",
-            "translate": "упорядочивать",
-            "step": 1,
-            "date": 0
+            'index': 'index11',
+            'word': 'bestellen',
+            'translate': 'упорядочивать',
+            'step': 1,
+            'date': 0
         }, {
-            "index": "index12",
-            "word": "kalt",
-            "translate": "холодный",
-            "step": 2,
-            "date": 0
+            'index': 'index12',
+            'word': 'kalt',
+            'translate': 'холодный',
+            'step': 2,
+            'date': 0
         }, {
-            "index": "index13",
-            "word": "sauer",
-            "translate": "кислый",
-            "step": 2,
-            "date": 0
+            'index': 'index13',
+            'word': 'sauer',
+            'translate': 'кислый',
+            'step': 2,
+            'date': 0
         }, {
-            "index": "index14",
-            "word": "fliegen",
-            "translate": "летать",
-            "step": 3,
-            "date": 0
+            'index': 'index14',
+            'word': 'fliegen',
+            'translate': 'летать',
+            'step': 3,
+            'date': 0
         }]
     );
 
--- app/data/loadWords-zh-rTW-en.js
+++ app/data/loadWords-zh-rTW-en.js
@@ -1,6 +1,6 @@
 /**************************************************
 * Learn Words // loadWords-zh-rTW-en.js
-* 
+*
 * Example of a user generated file
 *
 * Kersing Huang, November 2016
@@ -11,86 +11,85 @@
 if (LW.db.isEmpty()) {
 
   LW.db.loadWords([{
-      "word": "蘋果",
-      "translate": "apple"
+      'word': '蘋果',
+      'translate': 'apple'
     },
     {
-      "word": "桔子",
-      "translate": "orange"
+      'word': '桔子',
+      'translate': 'orange'
     },
     {
-      "word": "山莓",
-      "translate": "raspberry"
+      'word': '山莓',
+      'translate': 'raspberry'
     },
     {
-      "word": "草莓",
-      "translate": "strawberry"
+      'word': '草莓',
+      'translate': 'strawberry'
     },
     {
-      "word": "杏",
-      "translate": "apricot"
+      'word': '杏',
+      'translate': 'apricot'
     },
     {
-      "word": "椰子",
-      "translate": "coconut"
+      'word': '椰子',
+      'translate': 'coconut'
     },
     {
-      "word": "梨",
-      "translate": "pear"
+      'word': '梨',
+      'translate': 'pear'
     },
     {
-      "word": "凤梨",
-      "translate": "pineapple"
+      'word': '凤梨',
+      'translate': 'pineapple'
     },
     {
-      "word": "番石榴",
-      "translate": "guava"
+      'word': '番石榴',
+      'translate': 'guava'
     },
     {
-      "word": "香蕉",
-      "translate": "banana"
+      'word': '香蕉',
+      'translate': 'banana'
     },
     {
-      "word": "瓜",
-      "translate": "melon"
+      'word': '瓜',
+      'translate': 'melon'
     },
     {
-      "word": "檸檬",
-      "translate": "lemon"
+      'word': '檸檬',
+      'translate': 'lemon'
     },
     {
-      "word": "李子",
-      "translate": "plum"
+      'word': '李子',
+      'translate': 'plum'
     },
     {
-      "word": "葡萄",
-      "translate": "grape"
+      'word': '葡萄',
+      'translate': 'grape'
     },
     {
-      "word": "芒果",
-      "translate": "mango"
+      'word': '芒果',
+      'translate': 'mango'
     },
     {
-      "word": "青檸",
-      "translate": "lime"
+      'word': '青檸',
+      'translate': 'lime'
     },
     {
-      "word": "櫻桃",
-      "translate": "cherry"
+      'word': '櫻桃',
+      'translate': 'cherry'
     },
     {
-      "word": "奇異果",
-      "translate": "kiwi"
+      'word': '奇異果',
+      'translate': 'kiwi'
     },
     {
-      "word": "藍莓",
-      "translate": "blueberry"
+      'word': '藍莓',
+      'translate': 'blueberry'
     },
     {
-      "word": "黑莓",
-      "translate": "blackberry"
+      'word': '黑莓',
+      'translate': 'blackberry'
     }
   ]);
 
 }
-
